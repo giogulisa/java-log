c3e663437cfb6dda0f9a41d2db16d9732ecbc808
giorgi <g_gulisashvili@cu.edu.ge>
2019-02-27 18:48:34 +0400

    java

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import org.apache.log4j.Logger;
+
+public class Main {
+
+
+    private static final Logger log=Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        log.info("info vaar");
+        log.error("erori varr");
+
+
+
+    }
+}
